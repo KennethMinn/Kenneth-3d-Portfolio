d8124a2509f969da3159123801693348b3b99f62
KennethMinn <minthukha2472005@gmail.com>
2024-03-18 21:13:31 +0630

    fixed again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
       <div
         className={`${styles.paddingX}  inset-0 mt-[75px] max-w-7xl mx-auto flex flex-row items-start gap-5`} //absolute
       >
-        <div className=" flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col items-center justify-center mt-5 ">
           <div className=" w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className=" w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 h-40 sm:h-80 violet-gradient" />
         </div>
-        <div className=" mt-10">
+        <div className="mt-10 ">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I&apos;m <span className="text-[#915eff]">Kenneth</span>
           </h1>
@@ -25,12 +25,12 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className=" flex justify-center mt-[-90px]">
+      <div className=" flex justify-center mt-[-90px] bg-white">
         <Lottie animationData={computer} className=" h-[500px] md:h-[600px]" />
       </div>
 
       {/* <ComputersCanvas /> */}
-      {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      {/* <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
         <a href="#about">
           <div className=" w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
@@ -40,7 +40,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: 'loop',
               }}
-              className=" w-3 h-3 rounded-full bg-secondary mb-1"
+              className="w-3 h-3 mb-1 rounded-full bg-secondary"
             ></motion.dev>
           </div>
         </a>
