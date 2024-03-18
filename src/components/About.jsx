93b12e3ea7e28422814f873a130678ea1cecfc6f
KennethMinn <minthukha2472005@gmail.com>
2024-03-18 12:46:39 +0630

    fixed overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,11 +38,15 @@ const About = () => {
   const skills = [
     'HTML',
     'CSS',
-    'JS',
+    'JavaScript',
     'TypeScript',
     'ReactJs',
     'NextJs',
+    'Prisma',
+    'GraphQl',
+    'Rest',
     'Redux',
+    'Zustand',
     'React Query',
     'Php',
     'Laravel',
@@ -87,21 +91,23 @@ const About = () => {
                   <span className="text-red-500 ">return </span> {'['}
                 </div>
                 <div className=" ms-[20px]">
-                  {'{'}{' '}
-                  <span>
-                    internship : &apos;Developed Kanote web application together
-                    with the team at Tee Htwin&apos;
-                  </span>
-                  {'}'}
+                  {'{'} <br />
+                  <span className=" md:ms-4">
+                    frontend developer : &apos;Developed Kanote web application
+                    together with the team at Tee Htwin&apos;
+                  </span>{' '}
+                  <br />
+                  {'},'}
                 </div>
                 <div className=" ms-[20px]">
-                  {'{'}{' '}
-                  <span>
-                    full-time : &apos;Developed Korean Projects and
+                  {'{'} <br />
+                  <span className=" md:ms-4">
+                    frontend developer : &apos;Developed Korean Projects and
                     International Projects together with the team at ITWizard
                     {' ( Tagoplus )'}
                     &apos;
                   </span>
+                  <br />
                   {'}'}
                 </div>
                 <div>{']'}</div>
